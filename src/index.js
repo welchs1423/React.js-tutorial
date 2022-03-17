4acdbb629afd7e1a7263290108b79ac3b0f76338
welchs1423 <welchs1423@naver.com>
2022-03-17 19:05:23 +0900

    9편

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import MyReactApp from './App';    // 'App'은 다른 이름으로 변경 할수 있음.(첫 글자는 대문자 여야 함)
 // import * as serviceWorker from './serviceWorker';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <MyReactApp />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
