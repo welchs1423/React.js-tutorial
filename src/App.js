04ba0fe37c3dbe23d37cd72f62d55215f5037c70
welchs1423 <welchs1423@naver.com>
2022-03-24 14:16:13 +0900

    15.2 State 사용

--- src/App.js
+++ src/App.js
@@ -5,11 +5,20 @@ import Subject from "./components/Subject";
 import './App.css';
 
 class App extends Component {
+  constructor(props){ // 어떤 component가 실행 될때 render보다 먼저 실행 되면서 component를 초기화 시키기 위한 코드
+    super(props);
+    this.state = {
+      subject:{title:'WEB', sub:'World Wide Web!'}
+    }
+  }
   render() {
     return (  // "", \ 같은 것들을 안써도 되도록 페이스북에서 만든 언어 jsx
               // jsx를 코딩하면 create react app이 자바스크립트로 코딩 해준다.
       <div className="App">
-        <Subject title="WEB" sub="world wide web!"></Subject>
+        <Subject
+         title={this.state.subject.title}
+         sub={this.state.subject.sub}>
+        </Subject>
         <TOC></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
